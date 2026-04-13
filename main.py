5e2b59f5ec49b244de3f044f31aec73eda33b2b5
Mbrenneman0 <mbrenneman0@gmail.com>
2026-04-13 11:15:53 -0400

    added boilerplate code to main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from Modules.Interface import Client
+
+LOGIN = 'root'
+PASSWORD = 'cset155'
+SERVER = 'localhost'
+DB_NAME = 'ecom'
+SCHEMA_PATH = 'ecommDB.sql'
+
+client = Client(LOGIN, PASSWORD, SERVER, DB_NAME, SCHEMA_PATH)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
