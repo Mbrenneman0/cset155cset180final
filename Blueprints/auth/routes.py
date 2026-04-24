f3cda662f5650e00705743c90e54e06fc8157786
Aiden Rathsam <rathsamaiden@gmail.com>
2026-04-24 11:04:44 -0400

    login/register routes route to index when filled out :: inputs have generic input checks

--- Blueprints/auth/routes.py
+++ Blueprints/auth/routes.py
@@ -1,22 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint
+from flask import render_template, request, redirect, url_for, flash, Blueprint, session
+from Services.auth_service import parse_login_inputs, parse_register_inputs, create_user, create_session
 
 auth_bp = Blueprint('authenticate', __name__, url_prefix='/authenticate')
 
 @auth_bp.route('/login/username', methods=['GET','POST'])
 def login_username():
-    inputs = ['Username','Password']
+    input_types = ['Username','Password']
     if request.method == 'POST':
-        return
+        inputs = parse_login_inputs(request.form)
+        if len(inputs) != len(input_types):
+            return render_template('base_auth.html', title='Login', inputs=input_types, saved_inputs=inputs, log_type='Sign in with email instead? Click here!')
+        return redirect(url_for('index.index'))
     else:
-        return render_template('base_auth.html', title='Login', inputs=inputs, log_type='Sign in with email instead? Click here!')
+        return render_template('base_auth.html', title='Login', inputs=input_types, log_type='Sign in with email instead? Click here!')
     
 @auth_bp.route('/login/email', methods=['GET','POST'])
 def login_email():
+    input_types = ['Email','Password']
     if request.method == 'POST':
-        return
+        inputs = parse_login_inputs(request.form)
+        if len(inputs) != len(input_types):
+            return render_template('base_auth.html', title='Login', inputs=input_types, saved_inputs=inputs, log_type='Sign in with username instead? Click here!')
+        return redirect(url_for('index.index'))
     else:
-        inputs = ['Email','Password']
-        return render_template('base_auth.html', title='Login', inputs=inputs, log_type='Sign in with username instead? Click here!')
+        return render_template('base_auth.html', title='Login', inputs=input_types, log_type='Sign in with username instead? Click here!')
 
 @auth_bp.route('/logout', methods=['POST'])
 def logout():
@@ -24,16 +31,26 @@ def logout():
 
 @auth_bp.route('/register', methods=['GET','POST'])
 def register():
+    input_types = ['Name','Username','Email','Password']
     if request.method == 'POST':
-        return
+        inputs = parse_register_inputs(request.form)
+        if len(inputs) != len(input_types):
+            return render_template('base_auth.html', title='Register', inputs=input_types, saved_inputs=inputs)
+        # create_user(inputs)
+        return redirect(url_for('index.index'))
     else:
-        inputs = ['Name','Username','Email','Password']
-        return render_template('base_auth.html', title='Register', inputs=inputs)
+        
+        return render_template('base_auth.html', title='Register', inputs=input_types)
 
-@auth_bp.route('/vender/register', methods=['GET','POST'])
+@auth_bp.route('/register/vender', methods=['GET','POST'])
 def vender_register():
+    input_types = ['Name','Username','Email','Password']
     if request.method == 'POST':
-        return
+        inputs = parse_register_inputs(request.form)
+        if len(inputs) != 0:
+            return render_template('base_auth.html', title='Register', inputs=input_types, saved_inputs=inputs)
+        # create_user(inputs)
+        return redirect(url_for('index.index'))
     else:
-        inputs = ['Name','Username','Email','Password']
-        return render_template('base_auth.html', title='Register', inputs=inputs)
+        
+        return render_template('base_auth.html', title='Register', inputs=input_types)
--- Services/auth_service.py
+++ Services/auth_service.py
@@ -1,10 +1,47 @@
+from flask import flash, session
 import extensions
 from Modules.Types import *
 
-def validate_input(input:str) -> bool:
-    if input.isspace():
-        return False
-    return True
+def _does_input_exist(col:str, value:str) -> bool:
+    rslt = extensions.client.conn.get_rows(TableNames.USERS, condition=f'{col}=\"{value}\"')
+    return True if len(rslt) != 0 else False
 
-def compare_input() ->  bool:
-    return
+def check_credentials(role_type:str, user_id:str=session['user_id']) -> bool:
+    check = True
+    if not _does_input_exist('user_id', user_id):
+        check = False
+        raise KeyError(f'User ID not found at: {session['user_id']}')
+    if session['role'] != role_type:
+        check = False
+        raise KeyError(f'User at user_id :{session['user_id']}, does not have {role_type} access')
+    return check
+
+def parse_login_inputs(form_inputs) -> dict:
+    inputs = dict(form_inputs)
+    for key, value in inputs.items():
+        if value.isspace():
+            flash(f'Invalid {key} input. Please try again.')
+            inputs[key] = None
+        if _does_input_exist(key, value):
+            flash(f'{key.capitalize()} value already in use. Please try again.')
+            inputs[key] = None
+    return inputs
+
+def parse_register_inputs(form_inputs) -> dict:
+    inputs = dict(form_inputs)
+    for key, value in inputs.items():
+        if value.isspace():
+            flash(f'Invalid {key} input. Please try again.')
+            inputs[key] = None
+        if not _does_input_exist(key, value):
+            flash(f'{key.capitalize()} value not in use. Please try again.')
+            inputs[key] = None
+    return inputs
+
+def create_user(form_inputs):
+    rslt = extensions.client.conn.create_row(TableNames.USERS, form_inputs)
+    create_session(rslt['user_id'], rslt['role'])
+
+def create_session(user_id:str=None, role:str=None):
+    session['user_id'] = user_id
+    session['role'] = role
